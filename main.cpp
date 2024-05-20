bb5e1e0edfb7b5ccc21ea5a53d5277aa33723be2
Asadullah <140200762+Ich-Asadullah@users.noreply.github.com>
2024-05-20 17:58:43 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,204 +1,193 @@
 #include <iostream>
-
-
 #include "MoviesSystem.h"
-#include "User.h"
 
 using namespace std;
 
-int main(){
+void displayMainMenu() {
+    cout << "\n1- Watch a Movie\n"
+         << "2- Trending\n"
+         << "3- Top Rated\n"
+         << "4- Filter\n"
+         << "5- Recommended Movies\n"
+         << "6- Profile\n"
+         << "7- Exit\n";
+}
 
+void handleWatchMovie(MovieSystem* NexFlix, User* user, LinkedList* moviesList) {
+    int watchChoice;
+    cout << "1- Search by Movie ID\n"
+         << "2- Search By Movie Title\n";
+    cin >> watchChoice;
+
+    MovieNode* watched = nullptr;
+    switch (watchChoice) {
+        case 1: {
+            int movieIdInput;
+            cout << "\nEnter Movie ID\n";
+            cin >> movieIdInput;
+            watched = moviesList->searchMovie(movieIdInput);
+            break;
+        }
+        case 2: {
+            string movieTitleInput;
+            cout << "\nEnter Movie Title\n";
+            cin >> movieTitleInput;
+            watched = moviesList->search_by_name(movieTitleInput);
+            break;
+        }
+        default:
+            cout << "Invalid choice. Returning to main menu.\n";
+            return;
+    }
+
+    if (watched) {
+        watched->display();
+        user->updateWatchedMovies(user->userId, watched->title);
+
+        int afterWatch;
+        cout << ".........Watched........."
+             << "\n1- Add Reviews\n"
+             << "2- Report Movie\n"
+             << "3- Main Menu\n";
+        cin >> afterWatch;
+
+        switch (afterWatch) {
+            case 1: {
+                string reviewInput;
+                cout << "\nEnter review\n";
+                cin.ignore(); // Ignore the newline character left in the input buffer
+                getline(cin, reviewInput);
+                string review = "Movie ID: " + to_string(watched->movieId) + " Movie Title: " + watched->title + " Review: " + reviewInput;
+                user->reviewStack.addReview(review, user->userId);
+                cout << "Review Added.....\n";
+                break;
+            }
+            case 2:
+                moviesList->removeMovie(watched->movieId);
+                user->removeWatched(watched->title, user->userId);
+                cout << "Movie removed...........\nWe apologize for the inconvenience.....\n";
+                break;
+            default:
+                break;
+        }
+    } else {
+        cout << "Movie not found.\n";
+    }
+}
+
+void handleTrending(LinkedList* moviesList) {
+    cout << "\nTop 10 Trending Movies\n";
+    LinkedList* trending = moviesList->sort_by_rating();
+    trending->printLimited(10);
+    delete trending;
+}
+
+void handleTopRated(LinkedList* moviesList) {
+    cout << "\nTop Rated Movies\n";
+    LinkedList* topRated = moviesList->sort_by_votes();
+    topRated->printLimited(10);
+    delete topRated;
+}
+
+void handleFilter(LinkedList* moviesList) {
+    int filterType;
+    cout << "\n1- Filter by Genre\n"
+         << "2- Filter by Production\n";
+    cin >> filterType;
+
+    string filterInput;
+    switch (filterType) {
+        case 1:
+            cout << "\nEnter preferred Genre: ";
+            cin >> filterInput;
+            moviesList->print_by_genre(filterInput);
+            break;
+        case 2:
+            cout << "Enter preferred Production: ";
+            cin >> filterInput;
+            moviesList->print_by_production(filterInput);
+            break;
+        default:
+            cout << "Invalid filter type.\n";
+            break;
+    }
+}
+
+void handleRecommendedMovies(User* user, LinkedList* moviesList, RecommendationGraph& recommend) {
+    vector<string> userWatched = user->getWatchedMovies(user->userId);
+    vector<TreeNode*> recommendedGraph = recommend.getRecommendation(userWatched, moviesList);
+
+    RecommendationHeap recommendedMovies;
+    for (TreeNode* treeNode : recommendedGraph) {
+        recommendedMovies.insert(treeNode);
+    }
+
+    cout << "......Movies Based on your Activity......\n";
+    recommendedMovies.printHeapByPriority();
+}
+
+void handleProfile(User* user) {
+    cout << "Username: " << user->username;
+    cout << " ID: " << user->userId;
+    cout << "\nWatch History\n";
+    for (const string& movieName : user->getWatchedMovies(user->userId)) {
+        cout << movieName << "\n";
+    }
+
+    cout << "\nReviews\n";
+    user->reviewStack.displayReviews(user->userId);
+}
+
+int main() {
     MovieSystem* NexFlix = new MovieSystem();
     RecommendationGraph recommend;
 
     // Login or Signup
     User* user = nullptr;
-    while(!user){
+    while (!user) {
         user = NexFlix->authenticate();
     }
 
-    //Loading the data for Dataset
-    LinkedList* moviesList;
+    LinkedList* moviesList = new LinkedList();
     NexFlix->loadData(moviesList);
 
-    //Initialization of all variables that will be used
-    LinkedList* topRated;
-    LinkedList* trending;
-    MovieNode* watched;
-    vector<string> userWatched;
-    vector<TreeNode*> recommendedGraph;
-    RecommendationHeap recommendedMovies;
-    int mainChoice, watchChoice, movieIdInput, filterType;
-    string review, reviewInput, movieTitleInput, filterInput;
+    int mainChoice;
+    bool running = true;
 
-    //Main Menu
-    cout<<"1- Watch a Movie\n"
-        <<"2- Trending\n"
-        <<"3- Top Rated\n"
-        <<"4- Filter\n"
-        <<"5- Recommended Movies\n"
-        <<"6- Profile\n";
-    
-    cin>>mainChoice;
+    while (running) {
+        displayMainMenu();
+        cin >> mainChoice;
 
-    switch (mainChoice)
-    {
-    // Case To watch a Movie
-    case 1:
-        cout<<"1- Search by Movie ID\n"
-            <<"2- Search By Movie Title\n";
-        cin>>watchChoice;
-
-        switch (watchChoice)
-        {
-        case 1:
-            cout<<"Enter Movie ID\n";
-            cin>>movieIdInput;
-            watched = moviesList->searchMovie(movieIdInput);
-            // Check if movie is watched
-            if (watched){
-                int afterWatch;
-                cout<<".........Watched........."
-                    <<"1- Add Reviews\n"
-                    <<"2- Report Movie\n"
-                    <<"3- Main Menu\n";
-                cin>>afterWatch;
-
-                // Switch to Check Reviews Report or Main menu
-                switch (afterWatch)
-                {
-                //Adding review
-                case 1:
-                    cout<<"Enter review\n";
-                    cin>>reviewInput;
-                    review = "Movie ID: " + to_string(watched->movieId) + "Movie Title: " + watched->title + "\nReview: " + reviewInput;
-                    user->reviewStack.addReview(review, user->userId);
-                    cout<<"Review Added.....";
-                    break;
-                // Reporting and removing a movie
-                case 2:
-                    moviesList->removeMovie(watched->movieId);
-                    user->removeWatched(watched->title, user->userId);
-                    cout<<"Movie removed...........\nWe apologize for inconvinience.....";
-                // Main Menu
-                default:
-                    break;
-                }
-                }
-            // No Movie Found
-            else{
-                cout<<"Movie not fount with ID : "<<movieIdInput;
-                break;
-            }
-            break;
-        
-        case 2:
-            cout<<"Enter Movie Title\n";
-            cin>>movieTitleInput;
-            watched = moviesList->search_by_name(movieTitleInput);
-            // Check if movie is watched
-            if (watched){
-                int afterWatch;
-                cout<<".........Watched........."
-                    <<"1- Add Reviews\n"
-                    <<"2- Report Movie\n"
-                    <<"3- Main Menu\n";
-                cin>>afterWatch;
-
-                // Switch to Check Reviews Report or Main menu
-                switch (afterWatch)
-                {
-                    //Adding review
-                    case 1:
-                        reviewInput;
-                        cout<<"Enter review\n";
-                        cin>>reviewInput;
-                        review = "Movie ID: " + to_string(watched->movieId) + "Movie Title: " + watched->title + "\nReview: " + reviewInput;
-                        user->reviewStack.addReview(review, user->userId);
-                        cout<<"Review Added.....";
-                        break;
-                    // Reporting and removing a movie
-                    case 2:
-                        moviesList->removeMovie(watched->movieId);
-                        cout<<"Movie removed...........\nWe apologize for inconvinience.....";
-                    // Main Menu
-                    default:
-                        break;
-                    }
-                }
-            // No Movie Found
-            else{
-                cout<<"Movie not fount with Title : "<<movieTitleInput;
-                break;
-            }
-            break;
-        
-        default:
-            break;
-        }
-        break;
-    
-    case 2:
-        cout<<"\nTop 10 Trending Movies\n";
-        trending = moviesList->sort_by_rating();
-        trending->printLimited(10);
-        break;
-
-    case 3:
-        cout<<"\nTop Rated Movies\n";
-        topRated = moviesList->sort_by_votes();
-        topRated->printLimited(10);
-        break;
-    
-    case 4:
-        filterType;
-        cout<<"1- Filter by Genre\n"
-            <<"2- Filter by Production\n";
-
-        cin>>filterType;
-
-        switch (filterType)
-        {
-            filterInput;
+        switch (mainChoice) {
             case 1:
-                cout<<"Enter prefered Genre: ";
-                cin>>filterInput;
-                moviesList->print_by_genre(filterInput);
+                handleWatchMovie(NexFlix, user, moviesList);
                 break;
-            
             case 2:
-                cout<<"Enter prefered Production: ";
-                cin>>filterInput;
-                moviesList->print_by_production(filterInput);
+                handleTrending(moviesList);
                 break;
-            
+            case 3:
+                handleTopRated(moviesList);
+                break;
+            case 4:
+                handleFilter(moviesList);
+                break;
+            case 5:
+                handleRecommendedMovies(user, moviesList, recommend);
+                break;
+            case 6:
+                handleProfile(user);
+                break;
+            case 7:
+                running = false;
+                break;
             default:
+                cout << "Invalid choice. Please try again.\n";
                 break;
         }
-
-    case 5:
-        userWatched = user->getWatchedMovies(user->userId);
-        recommendedGraph = recommend.getRecommendation(userWatched, moviesList);
-
-        for (TreeNode* treeNode : recommendedGraph){
-            recommendedMovies.insert(treeNode);
-        }
-
-        cout<<"......Movies Based on your Activity......";
-        recommendedMovies.printHeapByPriority();
-    
-    case 6:
-        cout<<"Username: "<<user->username;
-        cout<<"ID: "<<user->userId;
-        cout<<"\nWatch History\n";
-        for (string movieName : user->getWatchedMovies(user->userId)){
-            cout<<movieName<<"\n";
-        } 
-
-        cout<<"\nReviews\n";
-        user->reviewStack.displayReviews(user->userId);
-
-    default:
-        break;
     }
+
+    delete moviesList;
+    delete NexFlix;
+
+    return 0;
 }
